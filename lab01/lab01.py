191050ad7b51fd3349c335743c415d05b0d97479
zouhui1024 <104685585+zouhui1024@users.noreply.github.com>
2025-11-12 10:20:11 +0800

    Add files via upload

---
+++ lab01/lab01.py
@@ -0,0 +1,125 @@
+def digit(n, k):
+    """Return the k-th digit from the right of n for positive integers n and k.
+
+    >>> digit(3579, 2)
+    5
+    >>> digit(3579, 0)
+    9
+    >>> digit(3579, 10)
+    0
+    """
+    return int(str(n)[-(k+1)]) if len(str(n))>=k  else 0  
+
+
+
+def middle(a, b, c):
+    """Return the number among a, b, and c that is not the smallest or largest.
+    Assume a, b, and c are all different numbers.
+
+    >>> middle(3, 5, 4)
+    4
+    >>> middle(30, 5, 4)
+    5
+    >>> middle(3, 5, 40)
+    5
+    >>> middle(3, 5, 40)
+    5
+    >>> middle(30, 5, 40)
+    30
+    """
+    return a if a not in [max(a,b,c),min(a,b,c)] else (b if b not in [max(a,b,c), min(a,b,c)] else c)
+
+
+def falling(n, k):
+    """Compute the falling factorial of n to depth k.
+
+    >>> falling(6, 3)  # 6 * 5 * 4
+    120
+    >>> falling(4, 3)  # 4 * 3 * 2
+    24
+    >>> falling(4, 1)  # 4
+    4
+    >>> falling(4, 0)
+    1
+    """
+    sum = 1
+    if k == 0:
+        return sum
+    else:
+        for i in range(k):
+            sum = sum * (n - i)
+        return sum
+        
+
+
+def divisible_by_k(n, k):
+    """
+    >>> a = divisible_by_k(10, 2)  # 2, 4, 6, 8, and 10 are divisible by 2
+    2
+    4
+    6
+    8
+    10
+    >>> a
+    5
+    >>> b = divisible_by_k(3, 1)  # 1, 2, and 3 are divisible by 1
+    1
+    2
+    3
+    >>> b
+    3
+    >>> c = divisible_by_k(6, 7)  # There are no integers up to 6 that are divisible by 7
+    >>> c
+    0
+    """
+    num = 0
+    for i in range(k, n+1):
+        if i / k >= 1 and i % k == 0:
+            print(i)
+            num += 1
+    return num
+
+
+
+def sum_digits(y):
+    """Sum all the digits of y.
+
+    >>> sum_digits(10) # 1 + 0 = 1
+    1
+    >>> sum_digits(4224) # 4 + 2 + 2 + 4 = 12
+    12
+    >>> sum_digits(1234567890)
+    45
+    >>> a = sum_digits(123) # make sure that you are using return rather than print
+    >>> a
+    6
+    """
+    sum = 0
+    y_str = str(y)
+    for i in y_str:
+        sum += int(i)
+    return sum
+
+
+
+def double_eights(n):
+    """Return true if n has two eights in a row.
+    >>> double_eights(8)
+    False
+    >>> double_eights(88)
+    True
+    >>> double_eights(2882)
+    True
+    >>> double_eights(880088)
+    True
+    >>> double_eights(12345)
+    False
+    >>> double_eights(80808080)
+    False
+    """
+    str_n = str(n)
+    if str_n.count('88') >= 1:
+        return True
+    else:
+        return False
+
